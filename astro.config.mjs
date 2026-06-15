6a6b6daf55c3356cbcd34a1ab11500d74d801f04
jerrySacort <jerry.dc37@gmail.com>
2026-06-15 01:37:30 -0600

    Correcion astroconfig y verecel json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: false }
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
